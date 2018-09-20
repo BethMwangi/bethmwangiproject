b8499995df54f7bd6051b4ae7993c5306380dbb8
BethMwangi <wanjikumwangib@gmail.com>
2018-09-21 00:25:09 +0300

    add sidebar items and styling

--- bethmwangi/src/App.js
+++ bethmwangi/src/App.js
@@ -5,69 +5,23 @@ import SideMenu  from './components/SideMenu';
 
 
 import './App.css';
-const { Header, Content, Footer, Sider } = Layout;
+
 
 class App extends Component {
   render() {
     return (
-      <Layout style={{ backgroundColor:'white' }}>
+      <div className='main'>
         <Row>
-        <Col style={{ backgroundColor: '' }} xs={6} sm={4} md={6} lg={8} xl={8}>
+        <Col
+         style={{
+          width: 400,
+          height: 700 }} xs={6} sm={4} md={6} lg={8} xl={8}>
       <SideMenu />
+
         </Col>
         <Col xs={18} sm={20} md={18} lg={16} xl={16}></Col>
         </Row>
-
-    {/* <Sider classname="nav-sider" style={{ overflow: 'auto', height: '100vh', position: 'fixed', left: 0 }}>
-      <div className="logo" />
-      <Menu className="navigation"  theme="" mode="inline" defaultSelectedKeys={['4']}>
-        <Menu.Item key="1">
-          <Icon type="user" />
-          <span className="nav-text">nav 1</span>
-        </Menu.Item>
-        <Menu.Item key="2">
-          <Icon type="video-camera" />
-          <span className="nav-text">nav 2</span>
-        </Menu.Item>
-        <Menu.Item key="3">
-          <Icon type="upload" />
-          <span className="nav-text">nav 3</span>
-        </Menu.Item>
-        <Menu.Item key="4">
-          <Icon type="bar-chart" />
-          <span className="nav-text">nav 4</span>
-        </Menu.Item>
-        <Menu.Item key="5">
-          <Icon type="cloud-o" />
-          <span className="nav-text">nav 5</span>
-        </Menu.Item>
-        <Menu.Item key="6">
-          <Icon type="appstore-o" />
-          <span className="nav-text">nav 6</span>
-        </Menu.Item>
-        <Menu.Item key="7">
-          <Icon type="team" />
-          <span className="nav-text">nav 7</span>
-        </Menu.Item>
-        <Menu.Item key="8">
-          <Icon type="shop" />
-          <span className="nav-text">nav 8</span>
-        </Menu.Item>
-      </Menu>
-    </Sider> */}
-    {/* <Layout style={{ marginLeft: 200 }}>
-      <Header style={{ background: '#fff', padding: 0 }} />
-      <Content style={{ margin: '24px 16px 0', overflow: 'initial' }}>
-        <div style={{ padding: 24, background: '#fff', textAlign: 'center', minHeight: 380  }}>
-
-          content
-        </div>
-      </Content>
-      <Footer style={{ margin: '24px 16px 0', padding: 24, background: '#fff', textAlign: 'center' , minHeight: 180  }}>
-       Footer
-      </Footer>
-    </Layout> */}
-  </Layout>
+  </div>
     );
   }
 }
--- bethmwangi/src/components/SideMenu.js
+++ bethmwangi/src/components/SideMenu.js
@@ -1,58 +1,86 @@
-import React, { Component } from 'react'
-import { Row, Col, Menu, Icon , Switch} from 'antd';
+import React, { Component } from "react";
+import { Row, Col, Menu, Icon, Switch } from "antd";
 const SubMenu = Menu.SubMenu;
 
 export default class SideMenu extends Component {
-    state = {
-        // theme:'dark',
-        current: '1',
-    }
-    changeTheme = (value) => {
-        this.setState({
-          theme: value ? 'dark' : 'light',
-        });
-      }
+  state = {
+    // theme:'dark',
+    current: "1"
+  };
+  changeTheme = value => {
+    this.setState({
+      theme: value ? "dark" : "light"
+    });
+  };
 
-      handleClick = (e) => {
-        console.log('click ', e);
-        this.setState({
-          current: e.key,
-        });
-      }
+  handleClick = e => {
+    console.log("click ", e);
+    this.setState({
+      current: e.key
+    });
+  };
   render() {
     return (
       <div>
-        <Row>
-            <Col>
 
-        <br />
 
-        <Menu
-        //   theme={this.state.theme}
-          onClick={this.handleClick}
-          style={{ width: 400, height: 700 , backgroundColor: '#000000', opacity: '0.6' , paddingLeft: '40%', paddingTop: '20%'}}
-        //   defaultOpenKeys={['sub1']}
-          selectedKeys={[this.state.current]}
-          mode="inline"
-        >
-          <Menu.Item
-          key="1">
-            <Icon type="mail" />
-            Navigation One
-          </Menu.Item>
-          <Menu.Item key="2">
-            <Icon type="calendar" />
-            Navigation Two
-          </Menu.Item>
-          <Menu.Item key="3">
-            <Icon type="calendar" />
-            Navigation Two
-          </Menu.Item>
-        </Menu>
+            <Menu
+              onClick={this.handleClick}
+              style={{
+                width: 400,
+                height: 700,
+                backgroundColor: "#000000",
+                opacity: "0.9",
+                paddingLeft: "40%",
+                paddingTop: "10%"
+              }}
+              //   defaultOpenKeys={['sub1']}
+              selectedKeys={[this.state.current]}
+              mode="inline"
+            >
 
-            </Col>
-        </Row>
+              <Menu.Item
+                style={{
+                  marginBotton: "10px",
+                  fontSize: '24px',
+                  fontWeight: 400
+                }}>
+            BETH MWANGI
+
+              </Menu.Item>
+              <Menu.Item>SOFTWARE DEVELOPER</Menu.Item>
+              <Menu.Item key="1">
+              <Icon type="home" />
+                ABOUT
+              </Menu.Item>
+
+              <Menu.Item key="2">
+              <Icon type="code" />
+                PROJECTS
+            </Menu.Item>
+              <Menu.Item key="3">
+              <Icon type="file-text" />
+                RESUME
+              </Menu.Item>
+              <Menu.Item key="4">
+              <Icon type="mail" />
+                CONTACT
+              </Menu.Item>
+
+              {/* <ul>
+                <li>
+                  Github
+                </li>
+                <li>
+                  Github
+                </li>
+              </ul> */}
+
+            </Menu>
+
+
+
       </div>
-    )
+    );
   }
 }
