33d97769ee4fc73ceb03d57b0ebe62d13c2c2ccc
BethMwangi <wanjikumwangib@gmail.com>
2018-10-03 12:05:08 +0300

    add contact me form

--- bethmwangi/src/App.js
+++ bethmwangi/src/App.js
@@ -1,27 +1,34 @@
-import React, { Component } from 'react';
-import { Layout, Row, Col, } from 'antd';
-import 'antd/dist/antd.css';
-import SideMenu  from './components/SideMenu';
+import React, { Component } from "react";
+import { Layout, Row, Col } from "antd";
+import "antd/dist/antd.css";
+import SideMenu from "./components/SideMenu";
+import Contact from "./components/Contact";
 
-
-import './App.css';
-
+import "./App.css";
 
 class App extends Component {
   render() {
     return (
-      <div className='main'>
+      <div className="main">
         <Row>
-        <Col
-         style={{
-          width: 400,
-          height: 700 }} xs={6} sm={4} md={6} lg={8} xl={8}>
-      <SideMenu />
-
-        </Col>
-        <Col xs={18} sm={20} md={18} lg={16} xl={16}></Col>
+          <Col
+            style={{
+              width: 400,
+              height: 700
+            }}
+            xs={6}
+            sm={4}
+            md={6}
+            lg={8}
+            xl={8}
+          >
+            <SideMenu />
+          </Col>
+          <Col xs={18} sm={20} md={18} lg={16} xl={16}>
+            <Contact />
+          </Col>
         </Row>
-  </div>
+      </div>
     );
   }
 }
